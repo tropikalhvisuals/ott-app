3bf6e79b699d70f4bf6d04cc793b8c0a33bf1684
tropikalhvisuals <tropikalhvisuals@gmail.com>
2026-04-22 14:12:14 +0530

    Update Signup.jsx

--- ott-react-app/src/pages/Signup.jsx
+++ ott-react-app/src/pages/Signup.jsx
@@ -84,7 +84,7 @@ function Signup() {
       setSubmitError("");
       console.log("Sending signup request...");
 
-      const response = await fetch("http://127.0.0.1:5000/api/signup", {
+      const response = await fetch("http://187.127.154.131:5000/api/signup", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
